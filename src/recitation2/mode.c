6c1ffb99cad1e2f316a13b816b6dac82d5f2e709
Ishan Pranav <ishan.pranav@outlook.com>
2023-02-06 12:05:26 -0500

    Add recitations 1 and 2

---
+++ src/recitation2/mode.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * The main entry point for the application.
+*/
+int main()
+{
+    srand(time(NULL));
+
+    int i;
+    int frequencies[10] = { 0 };
+
+    for (i = 0; i < 1000; i++)
+    {
+        frequencies[(rand() % 10)]++;
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Number: %d, Frequency: %d\n", i + 1, frequencies[i]);
+    }
+
+    int mode = 0;
+
+    for (i = 1; i < 10; i++)
+    {
+        if (frequencies[i] > frequencies[mode])
+        {
+            mode = i;
+        }
+    }
+
+    printf("Mode: %d\n", mode + 1);
+
+    return 0;
+}
