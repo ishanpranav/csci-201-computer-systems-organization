869e5fa5befca9341d33f2a3983fde3e23d796c9
Ishan Pranav <ishan.pranav@outlook.com>
2023-02-08 10:12:45 -0500

    Add problem 3 solution

--- src/project1/problem3.c
+++ src/project1/problem3.c
@@ -1,1 +1,43 @@
-// implement your program here
+#include <stdio.h>
+
+/**
+ * The main entry point for the application.
+ *
+ * @return An exit code. This value is always 0, indicating success.
+ */
+int main()
+{
+    int n;
+
+    while (scanf("%d", &n) && n)
+    {
+        int i = 1;
+        int j = 0;
+        int an = 0;
+        int bn = 0;
+
+        while (n)
+        {
+            if (n & 1)
+            {
+                if (i)
+                {
+                    an |= 1 << j;
+                    i = 0;
+                }
+                else
+                {
+                    bn |= 1 << j;
+                    i = 1;
+                }
+            }
+
+            j++;
+            n >>= 1;
+        }
+
+        printf("%d %d\n", an, bn);
+    }
+
+    return 0;
+}
