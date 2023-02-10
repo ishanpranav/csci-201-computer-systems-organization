c605db89b4898139f6ea517c2e6680243497de53
Ishan Pranav <ishan.pranav@outlook.com>
2023-02-10 10:50:11 -0500

    Add recitation 3

---
+++ src/recitation3/rec3.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+
+struct node
+{
+    char *word;
+    struct node *next;
+};
+
+void addFront(struct node *n, struct node **head);
+void reverse(struct node **head);
+void printNodes(struct node *head);
+void swapMiddle(struct node **head);
+
+int main()
+{
+    struct node *head = NULL;
+    struct node n1 = {"1", NULL};
+    struct node n2 = {"2", NULL};
+    struct node n3 = {"3", NULL};
+    struct node n4 = {"4", NULL};
+    struct node n5 = {"5", NULL};
+    struct node n6 = {"6", NULL};
+
+    addFront(&n1, &head);
+    addFront(&n2, &head);
+    addFront(&n3, &head);
+    addFront(&n4, &head);
+    addFront(&n5, &head);
+    addFront(&n6, &head);
+    printf("Values: ");
+    printNodes(head);
+    reverse(&head);
+    printf("Reversed: ");
+    printNodes(head);
+    reverse(&head);
+    printf("Reversed again (original): ");
+    printNodes(head);
+    printf("Swap middle: ");
+    swapMiddle(&head);
+    printNodes(head);
+    swapMiddle(&head);
+    printf("Swap middle again (original): ");
+    printNodes(head);
+
+    return 0;
+}
+
+void addFront(struct node *n, struct node **head)
+{
+    n->next = *head;
+    *head = n;
+}
+
+void printNodes(struct node *current)
+{
+    while (current != 0)
+    {
+        printf("%s ", current->word);
+        current = current->next;
+    }
+
+    printf("\n");
+}
+
+/**
+ * Reverses the elements of the linked list.
+ * 
+ * @param head a pointer to a pointer to a node structure; i.e., a head
+ *             reference.
+*/
+void reverse(struct node **head)
+{
+    if (head == NULL) {
+        return;
+    }
+
+    struct node *current = *head;
+
+    if (current == NULL) {
+        return;
+    }
+
+    struct node *previous = NULL;
+
+    // *[A] -> [B] -> [C] -> [D] -> [E]
+
+    // 1. *[A]
+    //     [B] -> [C] -> [D] -> [E]
+
+    // 2. [B] -> *[A]
+    //    [C]  -> [D] -> [E]
+
+    // 3. [C] -> [B] -> *[A]
+    //    [D] -> [E]
+
+    // 4. [D] -> [C] -> [B] -> *[A]
+    ///   [E]
+
+    // 5. [E] -> [D] -> [C] -> [B] -> *[A]
+
+    while (current != NULL) {
+        struct node *next = current->next;
+
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+
+    // *[E] -> [D] -> [C] -> [B] -> [A]
+
+    *head = previous;
+}
+
+/**
+ * Swaps the middle two elements of a linked list.
+ * 
+ * @param head a pointer to a pointer to a node structure; i.e., a head
+ *             reference.
+*/
+void swapMiddle(struct node **head)
+{
+    if (head == NULL) {
+        return;
+    }
+
+    // Step 1: Find the middle element
+    
+    //  Floyd's cycling finding algorithm: technique to find the middle
+    //  The 'slow' pointer iterates over every node; the 'fast' pointer
+    //  iterates over every other node. When the 'fast' (current) pointer
+    //  reaches the end, the 'slow' (middleRight) pointer must have reached
+    //  the middle (or the value to the right of it). 
+
+    struct node *middleLeft = NULL;
+    struct node *middleRight = *head;
+    struct node *current = *head;
+
+    if (middleRight == NULL || current == NULL) {
+        return;
+    }
+
+    while (current != NULL && current->next != NULL) {
+        middleLeft = middleRight;
+        middleRight = middleRight->next;
+        current = current->next->next;
+    }
+
+    // Step 2: Swap the middle elements (left and right)
+    
+    struct node *next = middleRight->next;
+
+    if (middleLeft == NULL) {
+        // *[middleRight] -> [middleRight.next] ...
+        
+        //  [middleRight]
+        // *[middleRight.next] ...
+    
+        *head = next;
+    } else {
+        // ... -> [middleLeft] -> [middleRight] -> [middleRight.next] -> [next] ...
+        
+        // [middleRight]
+        // ... -> [middleLeft] -> [middleRight.next] -> [next] ...
+
+        middleLeft->next = next;
+    }
+
+    // ... -> [middleRight] -> [middleLeft] -> [middleRight.next] -> [next]
+    
+    middleRight->next = next->next;
+    next->next = middleRight;
+}
