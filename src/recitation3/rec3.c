d722f63c77785d91e578062cf0beb08d74e00f63
Ishan Pranav <ishan.pranav@outlook.com>
2023-02-20 20:28:33 -0500

    Add quiz 1

--- src/recitation3/rec3.c
+++ src/recitation3/rec3.c
@@ -1,152 +1,5 @@
 #include <stdio.h>
-
-/**
- * Represents a linked-list node.
- */
-struct node
-{
-    /**
-     * Specifies the node value, a pointer to a zero-terminated string.
-     */
-    char *word;
-
-    /**
-     * Specifies the next node in the linked list. This value is 0 if the node
-     * is the tail of the linked list.
-     */
-    struct node *next;
-};
-
-/**
- * Adds a node to the front of a linked list.
- *
- * @param node a pointer to a node.
- * @param head a pointer to the head of a linked-list (itself a pointer to a
- *             node).
- */
-void addFront(struct node *node, struct node **head)
-{
-    node->next = *head;
-    *head = node;
-}
-
-/**
- * Reverses the elements of the linked list.
- *
- * @param head a pointer to the head of a linked-list (itself a pointer to a
- *             node).
- */
-void reverse(struct node **head)
-{
-    if (head == NULL)
-    {
-        return;
-    }
-
-    struct node *current = *head;
-
-    if (current == NULL)
-    {
-        return;
-    }
-
-    struct node *previous = NULL;
-
-    // *[A] -> [B] -> [C] -> [D] -> [E]
-
-    // 1. *[A]
-    //     [B] -> [C] -> [D] -> [E]
-
-    // 2. [B] -> *[A]
-    //    [C]  -> [D] -> [E]
-
-    // 3. [C] -> [B] -> *[A]
-    //    [D] -> [E]
-
-    // 4. [D] -> [C] -> [B] -> *[A]
-    ///   [E]
-
-    // 5. [E] -> [D] -> [C] -> [B] -> *[A]
-
-    while (current != NULL)
-    {
-        struct node *next = current->next;
-
-        current->next = previous;
-        previous = current;
-        current = next;
-    }
-
-    // *[E] -> [D] -> [C] -> [B] -> [A]
-
-    *head = previous;
-}
-
-/**
- * Prints a textual representation of a linked-list to the standard output
- * stream.
- *
- * @param current a pointer to the node from which to begin printing the list.
- */
-void printNodes(struct node *current)
-{
-    while (current != 0)
-    {
-        printf("%s ", current->word);
-        current = current->next;
-    }
-
-    printf("\n");
-}
-
-/**
- * Swaps the middle two elements of a linked list.
- *
- * @param head a pointer to the head of a linked-list (itself a pointer to a
- *             node).
- */
-void swapMiddle(struct node **head)
-{
-    //  Floyd's cycle-finding algorithm: technique to find the middle
-    //  The 'slow' pointer iterates over every node; the 'fast' pointer
-    //  iterates over every other node. When the 'fast' pointer reaches
-    //  the end, the 'slow'  pointer must have reached the middle.
-    
-    struct node *slow = *head;
-    struct node *fast = *head;
-
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    // There are an odd number of nodes
-
-    if (fast == NULL)
-    {
-        return;
-    }
-
-    // Find the previous node
-
-    struct node *previous = *head;
-    struct node *current = *head;
-
-    while (current != slow)
-    {
-        previous = current;
-        current = current->next;
-    }
-
-    // Perform the swap
-    
-    struct node *next = current->next;
-    
-    current->next = slow->next;
-    slow->next = next;
-    previous->next = slow;
-}
+#include "list_node.h"
 
 /**
  * The main entry point for the application.
@@ -155,34 +8,34 @@ void swapMiddle(struct node **head)
  */
 int main()
 {
-    struct node *head = NULL;
-    struct node n1 = {"1", NULL};
-    struct node n2 = {"2", NULL};
-    struct node n3 = {"3", NULL};
-    struct node n4 = {"4", NULL};
-    struct node n5 = {"5", NULL};
-    struct node n6 = {"6", NULL};
+    struct ListNode *head = NULL;
+    struct ListNode first = {"1", NULL};
+    struct ListNode second = {"2", NULL};
+    struct ListNode third = {"3", NULL};
+    struct ListNode fourth = {"4", NULL};
+    struct ListNode fifth = {"5", NULL};
+    struct ListNode sixth = {"6", NULL};
 
-    addFront(&n1, &head);
-    addFront(&n2, &head);
-    addFront(&n3, &head);
-    addFront(&n4, &head);
-    addFront(&n5, &head);
-    addFront(&n6, &head);
+    list_node_push(&first, &head);
+    list_node_push(&second, &head);
+    list_node_push(&third, &head);
+    list_node_push(&fourth, &head);
+    list_node_push(&fifth, &head);
+    list_node_push(&sixth, &head);
     printf("Values: ");
-    printNodes(head);
-    reverse(&head);
+    list_node_print(head);
+    list_node_reverse(&head);
     printf("Reversed: ");
-    printNodes(head);
-    reverse(&head);
+    list_node_print(head);
+    list_node_reverse(&head);
     printf("Reversed again (original): ");
-    printNodes(head);
+    list_node_print(head);
     printf("Swap middle: ");
-    swapMiddle(&head);
-    printNodes(head);
-    swapMiddle(&head);
+    list_node_swap_medians(&head);
+    list_node_print(head);
+    list_node_swap_medians(&head);
     printf("Swap middle again (original): ");
-    printNodes(head);
+    list_node_print(head);
 
     return 0;
 }
