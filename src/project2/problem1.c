368721db3a52f8eb78d362b4c083f1ad9c80af02
Ishan Pranav <ishan.pranav@outlook.com>
2023-03-21 22:17:37 -0400

    MODIFY THIS FILE

--- src/project2/problem1.c
+++ src/project2/problem1.c
@@ -1,7 +1,3 @@
-// Author: Ishan Pranav
-// Copyright (c) 2023 Ishan Pranav. All rights reserved. 
-// Licensed under the MIT License. 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
