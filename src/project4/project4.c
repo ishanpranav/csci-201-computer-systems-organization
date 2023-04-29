0923ae997a9fd89a3949bd9b86f277b28a8c29ac
Ishan Pranav <ishan.pranav@outlook.com>
2023-04-29 02:09:15 -0400

    Finalize project 4

--- src/project4/project4.c
+++ src/project4/project4.c
@@ -2,34 +2,83 @@
 // Copyright (c) 2023 Ishan Pranav. All rights reserved.
 // Licensed under the MIT License.
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define DEBUG
 #define BUFFER_SIZE 32
-#define DELIMITERS ",\n"
+#define DELIMITERS ", \t\n"
 #define DEFAULT_FILE_NAME "Project4Input (1) (1) (1).txt"
 
+/**
+ * Represents text as a sequence of characters.
+ */
 typedef char *String;
+
+/**
+ * Represents an 8-bit unsigned integer.
+ */
 typedef unsigned char Byte;
 
+/**
+ * Represents an entry in the translation lookaside buffer (TLB).
+ */
 typedef struct TlbEntry
 {
+    /**
+     * Specifies the tag of the entry.
+     */
     int tag;
+
+    /**
+     * Specifies the physical page number (PPN) of the entry.
+     */
     int physicalPageNumber;
 } TlbEntry;
 
+/**
+ * Represents an entry in the physical set-associative cache memory.
+ */
 typedef struct CacheLine
 {
+    /**
+     * Specifies the tag of the entry.
+     */
     int tag;
+
+    /**
+     * Specifies the physical bytes of cached memory.
+     */
     Byte bytes[4];
 } CacheLine;
 
+/**
+ * Specifies the input text buffer.
+ */
 char buffer[BUFFER_SIZE];
+
+/**
+ * Specifies an array representing the virtual page table.
+ */
 int pages[16];
+
+/**
+ * Specifies an array representing the physical cache memory.
+ */
 CacheLine cacheLines[16];
+
+/**
+ * Specifies a two-dimensional array representing the translation lookaside
+ * buffer (TLB).
+ */
 TlbEntry tlbSets[4][4];
 
+/**
+ * Retrieves the next token from the buffered tokenizer, converted to an
+ * integer from its hexadecimal string representation.
+ *
+ * @return The integral representation of the next buffered token.
+ */
 static int nextInt()
 {
     String token = strtok(NULL, DELIMITERS);
@@ -37,7 +86,10 @@ static int nextInt()
     return strtol(token, NULL, 16);
 }
 
-static bool read()
+/**
+ * Reads and parses the next line from the buffered tokenizer.
+ */
+static void read()
 {
     String token = strtok(buffer, DELIMITERS);
 
@@ -46,6 +98,8 @@ static bool read()
         int index = nextInt();
         int tag = nextInt();
         int physicalPageNumber = nextInt();
+
+        // Brief linear search to find an empty position in the TLB set
 
         for (int i = 0; i < 4; i++)
         {
@@ -60,6 +114,8 @@ static bool read()
     }
     else if (!strcmp(token, "Page"))
     {
+        // Index into the page table directly
+
         int virtualPageNumber = nextInt();
         int physicalPageNumber = nextInt();
 
@@ -67,6 +123,8 @@ static bool read()
     }
     else if (!strcmp(token, "Cache"))
     {
+        // Index into the cache directory
+
         int index = nextInt();
 
         cacheLines[index].tag = nextInt();
@@ -75,49 +133,6 @@ static bool read()
         {
             cacheLines[index].bytes[i] = nextInt();
         }
-    }
-
-    return true;
-}
-
-static void printError()
-{
-    printf("Can not be determined\n");
-}
-
-static void dumprt()
-{
-    for (int i = 0; i < 4; i++)
-    {
-        printf("TLB\t");
-
-        for (int j = 0; j < 4; j++)
-        {
-            printf("%02X %02X %02X\t", i, tlbSets[i][j].tag, tlbSets[i][j].physicalPageNumber);
-        }
-
-        printf("\n");
-    }
-
-    printf("\n");
-
-    for (int i = 0; i < 16; i++)
-    {
-        printf("Page\t%X %02X\n", i, pages[i]);
-    }
-
-    printf("\n");
-
-    for (int i = 0; i < 16; i++)
-    {
-        printf("Cache\t%X %02X", i, cacheLines[i].tag);
-
-        for (int j = 0; j < 4; j++)
-        {
-            printf(" %02X", cacheLines[i].bytes[j]);
-        }
-
-        printf("\n");
     }
 }
 
@@ -128,6 +143,9 @@ static void dumprt()
  */
 int main(int count, String args[])
 {
+    // If available, use the first command-line argument as the file name;
+    // otherwise, open the sample file.
+
     String fileName;
 
     if (count > 1)
@@ -139,18 +157,17 @@ int main(int count, String args[])
         fileName = DEFAULT_FILE_NAME;
     }
 
-    printf("Reading database from \"%s\"... ", fileName);
+    // Read from file (not standard input) as indicated by Professor
 
     FILE *streamReader = fopen(fileName, "r");
     int virtualAddress = 0;
 
-    while (fgets(buffer, BUFFER_SIZE, streamReader) && read())
-        ;
+    while (fgets(buffer, BUFFER_SIZE, streamReader))
+    {
+        read();
+    }
 
     fclose(streamReader);
-
-    printf("Done.\n");
-    dumprt();
 
     for (int round = 1; round <= 3; round++)
     {
@@ -161,11 +178,14 @@ int main(int count, String args[])
             break;
         }
 
+        int virtualPageNumber = virtualAddress >> 6;
         int tlbTag = (virtualAddress >> 8) & 0x3f;
-        int tlbIndex = (virtualAddress >> 6) & 0x3;
+        int tlbIndex = virtualPageNumber & 0x3;
         int cacheIndex = (virtualAddress >> 2) & 0xf;
         int cacheOffset = virtualAddress & 0x3;
         int physicalPageNumber = -1;
+
+        // Brief linear search to find a matching tag in the TLB set
 
         for (int i = 0; i < 4; i++)
         {
@@ -179,17 +199,22 @@ int main(int count, String args[])
 
         if (physicalPageNumber == -1)
         {
-            printError();
+            // No matching tag discovered in the TLB
+            // Index into the virtual page table instead
 
-            continue;
+            physicalPageNumber = pages[virtualPageNumber];
         }
 
         if (cacheLines[cacheIndex].tag != physicalPageNumber)
         {
-            printError();
-            
+            // Incorrect tag discovered in the cache
+
+            printf("Can not be determined\n");
+
             continue;
         }
+
+        // No issues encountered
 
         printf("%X\n", cacheLines[cacheIndex].bytes[cacheOffset]);
     }
