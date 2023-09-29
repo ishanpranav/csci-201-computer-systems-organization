164059cc34542cb44156382830d6b908d8f91497
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2023-09-29 00:19:08 -0400

    Update bar.c

--- src/project3/bar.c
+++ src/project3/bar.c
@@ -6,22 +6,22 @@
 
 long bar(long a, long b)
 {
-    long rdx = a;         // mov %rdi, %rdx
-    long result = 0;      // mov $0,   %eax
-    long rcx = 11 * a;    // lea (%rdi, %rdi, 4), %rcx 
-                          // lea (%rdi, %rcx, 2), %rcx
+    long rdx = a;           // mov %rdi, %rdx
+    long result = 0;        // mov $0,   %eax
+    long rcx = 11 * a;      // lea (%rdi, %rdi, 4), %rcx 
+                            // lea (%rdi, %rcx, 2), %rcx
 
-    a = b * 16;           // mov %rsi, %rdi
-                          // shl $4,   %rdi
+    a = b * 16;             // mov %rsi, %rdi
+                            // shl $4,   %rdi
     rcx += (b * 2) - a + 1; // lea (%rcx, %rsi, 2), %rcx
-                          // sub %rdi, %rcx
-                          // add $1,   %rcx
+                            // sub %rdi, %rcx
+                            // add $1,   %rcx
 
-    while (rdx <= b)      // cmp %rsi, %rdx
+    while (rdx <= b)        // cmp %rsi, %rdx
     {
-        rdx++;            // add $1,   %rdx
-        result += rcx;    // add %rcx, %rax
+        rdx++;              // add $1,   %rdx
+        result += rcx;      // add %rcx, %rax
     }
 
-    return result;        // repz ret
+    return result;          // repz ret
 }
